a7866ee94eb2cafaafc6254ca493d5ea97f47953
Chris Crump <chris@cypheros.co>
2019-09-30 16:47:42 -0400

    SystemUI: Introduce user interface for Alert Sliders
    Ported from OxygenOS and reworked for our alert slider
    implementation. We target AudioManager instead of Zen,
    icons are also the same as aosp and the dialog uses the
    material theme as well as support for our themes.
    
    To use, the alert slider config must be enabled. By default,
    the dialog shows on the left side. To move it to the right
    side, set the location config to 1.
    
    Change-Id: Ie1954a44cc5242c95a731abd7404379ea638fe70
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogComponent.java
@@ -40,6 +40,8 @@ import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.tristate.TriStateUiController;
+import com.android.systemui.tristate.TriStateUiControllerImpl;
 import com.android.systemui.tuner.TunerService;
 
 import java.io.PrintWriter;
