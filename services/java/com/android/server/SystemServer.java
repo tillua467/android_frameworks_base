429d2677e75a5eff065cdd3557de16075fa1d14c
Ido Ben-Hur <idoybh2@gmail.com>
2023-04-16 02:38:00 +0300

    \base: Introduce PowerOffAlarmService
    A service that makes use of qcom's power off alarm feature
    Eliminates the need of app specific implementation for said feature
    Should work with any alarm app that uses AlarmManager.setAlarmClock

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -239,6 +239,7 @@ import com.android.server.policy.PermissionPolicyService;
 import com.android.server.policy.PhoneWindowManager;
 import com.android.server.policy.role.RoleServicePlatformHelperImpl;
 import com.android.server.power.PowerManagerService;
+import com.android.server.power.PowerOffAlarmService;
 import com.android.server.power.ShutdownThread;
 import com.android.server.power.ThermalManagerService;
 import com.android.server.power.hint.HintManagerService;
@@ -1818,6 +1819,9 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(TradeInModeService.class);
                 t.traceEnd();
             }
+            t.traceBegin("StartPowerOffAlarmService");
+            mSystemServiceManager.startService(PowerOffAlarmService.class);
+            t.traceEnd();
 
             if (context.getResources().getBoolean(R.bool.config_reduceBrightColorsAvailable)) {
                 t.traceBegin("AutoDimService");
