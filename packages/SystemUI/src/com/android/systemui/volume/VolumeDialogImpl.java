b6b27d0fc176752145418fc4d1dc8254b0feb4b1
Ghosuto <clash.raja10@gmail.com>
2023-06-02 15:33:39 +0000

    SystemUI: Add haptic feedback to volume slider [1/2]
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Change-Id: I1cad42e6f99a961f9092ae75dda9cebc72f97bbf
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -3261,6 +3261,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+           mController.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_TEXTURE_TICK));
         }
 
         @Override
