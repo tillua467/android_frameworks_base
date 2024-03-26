cb1daf17192e26b30c9a595a326543091eb53bd3
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-26 09:27:10 +0800

    services: Introduce MatrixxServicesStarter
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Kshitij Koyande <koyande72@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -312,6 +312,8 @@ import com.android.server.wm.WindowManagerService;
 
 import dalvik.system.VMRuntime;
 
+import org.matrixx.server.MatrixxServicesStarter;
+
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -3213,6 +3215,9 @@ public final class SystemServer implements Dumpable {
         t.traceBegin("HealthConnectManagerService");
         mSystemServiceManager.startService(HEALTHCONNECT_MANAGER_SERVICE_CLASS);
         t.traceEnd();
+
+        MatrixxServicesStarter matrixxServiceStarter = new MatrixxServicesStarter(mSystemServiceManager);
+        matrixxServiceStarter.startAllServices();
 
         if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_DEVICE_LOCK)) {
             t.traceBegin("DeviceLockService");
