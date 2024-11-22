4fa475979289660ced196f1ad2157cce62837f8a
darkphnx <dark.phnx12@gmail.com>
2024-11-22 10:54:03 +0000

    Revert "Camera2: Notify fps as Session Based Parameter"
    This reverts commit 730b172039cd9a96ccd50c2f937fb67cacdb5a9f.

--- core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
+++ core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -126,8 +126,6 @@ public class CameraDeviceImpl extends CameraDevice
     @EnabledSince(targetSdkVersion = Build.VERSION_CODES.VANILLA_ICE_CREAM)
     static final long CHECK_PARAMS_IN_IS_SESSION_CONFIGURATION_SUPPORTED = 320741775;
 
-    private int customOpMode = 0;
-
     // TODO: guard every function with if (!mRemoteDevice) check (if it was closed)
     private ICameraDeviceUserWrapper mRemoteDevice;
     private boolean mRemoteDeviceInit = false;
