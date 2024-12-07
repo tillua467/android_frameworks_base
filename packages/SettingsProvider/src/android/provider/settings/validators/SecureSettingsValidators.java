f7f0f4b0e030dc82ac2e3669259c0220b2316183
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-07 10:14:03 +0900

    base: Add support for hide applist [1/2]
    * Based on commit "Hide hidden apps from all apps except system"
    * Hide user selected apps from applist
    * The applist includes system apps (including overlays), and user apps
    
    Ref:
    https://review.calyxos.org/c/CalyxOS/platform_frameworks_base/+/16571
    https://github.com/TheParasiteProject/frameworks_base/commit/6c41a0fc2c7ba01fd81132ceb0f3c1476099cf25
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
    Change-Id: If6c94fd345c488edab11927c4f567bc2171f69b2
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SecureSettingsValidators.java
@@ -453,5 +453,6 @@ public class SecureSettingsValidators {
                 new InclusiveIntegerRangeValidator(0, 1));
         VALIDATORS.put(Secure.ADVANCED_PROTECTION_MODE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(Secure.MEDIA_CONTROLS_ALWAYS_SHOW_TIME, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Secure.HIDE_APPLIST, ANY_STRING_VALIDATOR);
     }
 }
