fa4ce5219a8bdb305c51d864273ce012ffbe664a
Kshitij Koyande <koyande72@gmail.com>
2025-03-31 11:13:08 +0530

    Revert "SystemUI: Implement Split notification shade changes [SQUASH]"
    This reverts commit 1cf1991c0b806cb78aea21903d5442a679be4d9a.

--- packages/SystemUI/src/com/android/systemui/scene/ui/view/WindowRootView.kt
+++ packages/SystemUI/src/com/android/systemui/scene/ui/view/WindowRootView.kt
@@ -111,13 +111,11 @@ open class WindowRootView(
             if (child.layoutParams is LayoutParams) {
                 val layoutParams = child.layoutParams as LayoutParams
                 if (
-                    layoutParams.rightMargin != rightInset ||
-                            layoutParams.leftMargin != leftInset
+                    !layoutParams.ignoreRightInset &&
+                        (layoutParams.rightMargin != rightInset ||
+                            layoutParams.leftMargin != leftInset)
                 ) {
-                    layoutParams.updateMargins(
-                        left = if (layoutParams.ignoreLeftInset) 0 else leftInset,
-                        right = if (layoutParams.ignoreRightInset) 0 else rightInset
-                    )
+                    layoutParams.updateMargins(left = leftInset, right = rightInset)
                     hasChildMarginUpdated = true
                     if (!hasFlagsEnabled) {
                         child.requestLayout()
@@ -153,7 +151,6 @@ open class WindowRootView(
 
     private class LayoutParams : FrameLayout.LayoutParams {
         var ignoreRightInset = false
-        var ignoreLeftInset = false
 
         constructor(
             width: Int,
@@ -176,11 +173,6 @@ open class WindowRootView(
             ignoreRightInset =
                 obtainedAttributes.getBoolean(
                     R.styleable.StatusBarWindowView_Layout_ignoreRightInset,
-                    false
-                )
-            ignoreLeftInset =
-                obtainedAttributes.getBoolean(
-                    R.styleable.StatusBarWindowView_Layout_ignoreLeftInset,
                     false
                 )
             obtainedAttributes.recycle()
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -30,7 +30,6 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.annotation.IntDef;
 import android.content.Context;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.provider.Settings;
 import android.os.Handler;
@@ -268,7 +267,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     private Callback mCallback;
     private boolean mScreenOn;
     private boolean mTransparentScrimBackground;
-    private boolean mIsLandscape;
 
     // Scrim blanking callbacks
     private Runnable mPendingFrameCallback;
@@ -290,7 +288,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
                 mNotificationsAlpha = alphas.getNotificationsAlpha();
                 mNotificationsScrim.setViewAlpha(mNotificationsAlpha);
-                updateNotificationScrimVisibility();
 
                 mBehindAlpha = alphas.getBehindAlpha();
                 mScrimBehind.setViewAlpha(mBehindAlpha);
@@ -381,13 +378,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             @Override
             public void onUiModeChanged() {
                 ScrimController.this.onThemeChanged();
-            }
-
-            @Override
-            public void onConfigChanged(Configuration newConfig) {
-                int orientation = newConfig.orientation;
-                mIsLandscape = orientation == Configuration.ORIENTATION_LANDSCAPE;
-                updateNotificationScrimVisibility();
             }
         });
         mColors = new GradientColors();
@@ -977,9 +967,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                     mBehindAlpha = mCustomScrimAlpha;
                     mNotificationsAlpha = behindFraction * mCustomScrimAlpha;
                 } else {
-                    mBehindAlpha = mPanelExpansionFraction * mCustomScrimAlpha;
+                    mBehindAlpha = mLargeScreenShadeInterpolator.getBehindScrimAlpha(
+                            mPanelExpansionFraction * mDefaultScrimAlpha);
                     mNotificationsAlpha =
-                            mPanelExpansionFraction * mCustomScrimAlpha;
+                            mLargeScreenShadeInterpolator.getNotificationScrimAlpha(
+                                    mPanelExpansionFraction);
                 }
                 mBehindTint = mState.getBehindTint();
                 mInFrontAlpha = 0;
@@ -1585,12 +1577,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             state.setSurfaceColor(surface);
         }
 
-        // hide the surface on landscape 
-        final boolean showSurface = mUseDualToneColor && !mIsLandscape;
-        mBehindColors.setMainColor(showSurface ? surface : background);
+        mBehindColors.setMainColor(mUseDualToneColor ? surface : background);
         mBehindColors.setSecondaryColor(accent);
         final boolean isSurfaceBackgroundLight = !ContrastColorUtil.isColorDark(surface);
-        mBehindColors.setSupportsDarkText(showSurface ? isSurfaceBackgroundLight : isBackgroundLight);
+        mBehindColors.setSupportsDarkText(mUseDualToneColor ? isSurfaceBackgroundLight : isBackgroundLight);
 
         mNeedsDrawableColorUpdate = true;
     }
@@ -1685,12 +1675,6 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         for (ScrimState state : ScrimState.values()) {
             state.setLaunchingAffordanceWithPreview(launchingAffordanceWithPreview);
         }
-    }
-    
-    private void updateNotificationScrimVisibility() {
-        if (mNotificationsScrim == null) return;
-        mNotificationsScrim.setVisibility(
-            mIsLandscape ? View.GONE : View.VISIBLE);
     }
 
     public interface Callback {
