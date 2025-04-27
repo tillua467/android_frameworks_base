4fe432516c823fd995ce464cb6ffaf65516562a4
drkphnx <dark.phnx12@gmail.com>
2025-04-27 18:08:54 +0000

    Revert "fixup!"
    This reverts commit 13c2a9a4d2b371717e4e3ba79ec557083f477d66.

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -309,7 +309,6 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
         mFullCircleDrawable.setCharging(isCharging);
         mRLandscapeDrawable.setCharging(isCharging());
         mLandscapeDrawable.setCharging(isCharging());
-        mLandscapeDrawableiOS16.setCharging(isCharging());
         mAccessorizedDrawable.setBatteryLevel(level);
         mCircleDrawable.setBatteryLevel(level);
         mFullCircleDrawable.setBatteryLevel(level);
