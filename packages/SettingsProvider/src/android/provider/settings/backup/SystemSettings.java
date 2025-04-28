TenX-UDC/frameworks_base

SystemUI: Add haptic feedback to volume slider [1/2]
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Change-Id: I1cad42e6f99a961f9092ae75dda9cebc72f97bbf
Signed-off-by: Ghosuto <clash.raja10@gmail.com>
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -115,6 +115,7 @@ public class SystemSettings {
                 Settings.System.NAVIGATION_BAR_IME_SPACE,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
+                Settings.System.HAPTIC_ON_SLIDER,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {

tillua467/android_frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -120,7 +120,8 @@ public class SystemSettings {
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
                 Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
-                Settings.System.PREFERRED_REGION
+                Settings.System.PREFERRED_REGION,
+                Settings.System.HAPTIC_ON_SLIDER
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);